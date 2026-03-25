9b93d82dbd4afc185e400c4c8f1f6572e96b916d
Paula Perez <paulandreaperez14@gmail.com>
2026-03-25 10:47:20 -0500

    reverse proxy

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,13 +6,7 @@ declare global {
   var __prisma__: PrismaClient | undefined
 }
 
-const prisma = global.__prisma__ ?? new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-})
+const prisma = global.__prisma__ ?? new PrismaClient()
 if (process.env.NODE_ENV !== 'production') global.__prisma__ = prisma
 
 export default prisma
