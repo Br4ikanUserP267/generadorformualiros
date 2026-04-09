495215db81bf7bcff4d862a212c9fae033031e8c
Paula Pérez <paulandreaperez14@gmail.com>
2026-04-09 14:36:42 -0500

    fix: add basePath prefix to all apiFetch API calls

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,7 +12,23 @@ type ApiFetchOptions = RequestInit & {
 export async function apiFetch(input: RequestInfo | URL, options: ApiFetchOptions = {}) {
   const { redirectOn401 = true, ...init } = options
 
-  const response = await fetch(input, {
+  // When basePath is configured in next.config, fetch() calls don't automatically prefix it
+  // We need to manually add the basePath for all API routes
+  let url: string
+  if (input instanceof URL) {
+    url = input.href
+  } else {
+    url = input as string
+  }
+
+  // Add basePath prefix if this is a relative API path
+  if (typeof window !== 'undefined' && url.startsWith('/api') && !url.startsWith('/matriz-riesgos')) {
+    // Use the public base path constant
+    const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/matriz-riesgos'
+    url = `${basePath}${url}`
+  }
+
+  const response = await fetch(url, {
     credentials: 'include',
     ...init,
   })
